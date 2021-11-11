a40f30fbd9392ad7147617bcc6274ad20aabf298
Julie Breuil <jbpub@posteo.net>
2021-11-11 16:26:27 +0100

    ajout diverses

--- scenario/scenario.go
+++ scenario/scenario.go
@@ -4,13 +4,16 @@ type Scenario struct {
 	Carte *Carte
 	// agents
 	Agent *Agent
-	// taches
+	// liste de taches
+	
 	// parametres
 }
 
 type Task interface {
 	a()
 }
+
+
 
 type MoveTask struct {
 	goal Position
