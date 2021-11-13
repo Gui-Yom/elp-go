13542262a62a2c3c95741112154cf39ee659c63c
Julie Breuil <jbpub@posteo.net>
2021-11-13 16:34:26 +0100

    correction erreur dans agent.go

--- scenario/agent.go
+++ scenario/agent.go
@@ -7,9 +7,9 @@ type Agent struct {
 
 //boucle qui lit la liste de tâches
 func findTask(s Scenario) {
-	for ; s.Task.len==0 ;  {
+	//for ; s.Task.len==0 ;  {
 		
-	}
+	//}
 	//j'essaie de trouver une tâche
 	//je la fais
 	//je la coche
